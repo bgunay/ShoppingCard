1e0ab0ef066465d7f8f19f1caaa8846b0dc9be1b
b_gunay <b.gunay1@gmail.com>
2016-08-30 23:49:58 +0300

    test

---
+++ src/test/java/com/burhan/test/webstore/frontend/SeleniumLoginFrontendTest.java
@@ -0,0 +1,40 @@
+package com.burhan.test.webstore.frontend;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumLoginFrontendTest {
+
+    private WebDriver browser;
+
+    @Before
+    public void setup() {
+        browser = new FirefoxDriver();
+    }
+
+    @Test
+    public void startTest() {
+        browser.get("http://localhost:8080/ShoppingCard/login");
+
+
+        // Will throw exception if elements not found
+        browser.findElement(By.id("username")).sendKeys("admin");
+        browser.findElement(By.id("password")).sendKeys("admin");
+
+        browser.findElement(By.id("loginBtn")).click();
+        browser.findElement(By.id("account")).click();
+
+        assertEquals("John", browser.findElement(By.id("name")).getAttribute("value"));
+    }
+
+    @After
+    public void tearDown() {
+        browser.close();
+    }
+}
